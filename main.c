49f5cbd5db4b08dc4b1104999ac45ebf9c252056
Claudine-CS <claudineconceicao0@gmail.com>
2025-05-29 14:36:16 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+void limparBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+int main() {
+    srand(time(NULL));
+    int i, quantidade;
+    char = palavra, letra;
+    char lista [6] = {
+        "pinguim", "dinossauro", "jaguatirica", "toupeira", "tamanduá", "porco", "camelo"
+    };
+    
+    
+
+    
+    
+
+    return 0;
+}

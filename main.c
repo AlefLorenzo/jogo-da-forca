8547b18e19b97a5498a40a5a8405986f1ca267d4
AlefLorenzo <144935095+AlefLorenzo@users.noreply.github.com>
2025-05-29 23:06:53 -0300

    Update main.c

--- main.c
+++ main.c
@@ -21,3 +21,74 @@ int main() {
     
     return 0;
 }
+
+
+
+//teste
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void mostraForca(int error){
+    printf("+-------+");
+    printf("|       %c", error >= 1 ? 'O' : '_');
+}
+
+int main(){
+    char  palavras[5][20] = {
+        "celular",
+        "maripousa",
+        "normas",
+        "fantasia",
+        "heroi"
+    };  
+    char letra, resposta[20];
+    int erro = 0, acerto = 0;
+    int tamanho, escolha;
+    
+    srand(time(NULL));
+    escolha = rand() % 5;
+    char palavra[20];
+    
+    strcpy(palavra, palavras[escolha]);
+    
+    tamanho = strlen(palavra);
+    
+    for(int i = 0; i < tamanho; i++){
+        resposta[i] = '-';
+    }
+    resposta[tamanho] = '\0';
+    
+    printf("jogo da forca\n");
+    
+    while(erro < 6 && acerto < tamanho){
+        printf("palavras: \n");
+        for(int i = 0; i < tamanho; i++){
+            printf("%c",resposta[i]);
+        }
+        printf("\nnumero de tentativas restante: %d\n", 6 -  erro);
+        printf("digite uma letra: ");
+        scanf("%c", &letra);
+    }
+
+    for(int i = 0; i < tamanho; i++){
+        if(palavra[i] == letra &&resposta[i] == '-' ){
+            resposta[i] = letra; 
+            acerto++;
+            acerto = 1 ;
+        }
+    }
+    if(!acerto){
+        printf("nao acertou.\n");
+    }else{
+        printf("acertouuuuuuu.\n");
+    }
+    if(acerto == tamanho){
+        printf("parabens acertou: %s\n", palavra);
+    }else{
+        printf("perdeu! a palavra era : %s\n", palavra);
+    }
+
+    return 0;
+}

c4d5288c5f9ef454797fb1537a6c4eee99c04477
AlefLorenzo <144935095+AlefLorenzo@users.noreply.github.com>
2025-06-02 13:43:00 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,93 +1,88 @@
 #include <stdio.h>
-#include <locale.h>
 #include <stdlib.h>
-#include <time.h>
-#include <ctype.h>
+#include <string.h>
 
-void limparBuffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
+
+void mostra_boneco(int erro) {
+    printf(" +-------+\n");
+    printf(" |       %c\n", erro >= 1 ? 'O' : ' ');
+    printf(" |      %c%c%c\n", 
+           erro >= 3 ? '/' : ' ',
+           erro >= 2 ? '|' : ' ',
+           erro >= 4 ? '\\' : ' ');
+    printf(" |      %c %c\n", 
+           erro >= 5 ? '/' : ' ',
+           erro >= 6 ? '\\' : ' ');
+    printf(" |\n");
+    printf("_|_\n\n");
 }
 
 int main() {
-    srand(time(NULL));
-    int i, quantidade;
-    char palavra, letra;
-    char lista[6][20] = {
-        "pinguim", "dinossauro", "jaguatirica", "toupeira", "tamanduá", "porco"
-    };
+    int vida = 5;
+    char dica[20];
+    char frase[200];
+    char palavra[20], lacuna[20] = {0};
+    int tamanho, v = 0;
+    char letra;
     
     
     
-    return 0;
-}
+    
+    printf("Digite a palavra secreta: ");
+    fgets(palavra, sizeof(palavra), stdin);
+    palavra[strcspn(palavra, "\n")] = '\0'; 
 
-
-
-//teste
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-
-void mostraForca(int error){
-    printf("+-------+");
-    printf("|       %c", error >= 1 ? 'O' : '_');
-}
-
-int main(){
-    char  palavras[5][20] = {
-        "celular",
-        "maripousa",
-        "normas",
-        "fantasia",
-        "heroi"
-    };  
-    char letra, resposta[20];
-    int erro = 0, acerto = 0;
-    int tamanho, escolha;
-    
-    srand(time(NULL));
-    escolha = rand() % 5;
-    char palavra[20];
-    
-    strcpy(palavra, palavras[escolha]);
-    
+    printf("Digite a dica: ");
+    fgets(dica, sizeof(dica), stdin);
+    dica[strcspn(dica, "\n")] = '\0'; 
     tamanho = strlen(palavra);
-    
-    for(int i = 0; i < tamanho; i++){
-        resposta[i] = '-';
-    }
-    resposta[tamanho] = '\0';
-    
-    printf("jogo da forca\n");
-    
-    while(erro < 6 && acerto < tamanho){
-        printf("palavras: \n");
-        for(int i = 0; i < tamanho; i++){
-            printf("%c",resposta[i]);
+
+    while (vida > 0 && v == 0) {
+        system("clear"); 
+
+        printf("Dica: %s\n", dica);
+        printf("Vida: %d\n", vida);
+        
+        mostra_boneco(5 - vida);
+        
+        printf("Quantidade de letras: %d\n", tamanho);
+
+        for (int i = 0; i < tamanho; i++) {
+            if (lacuna[i] == '\0') printf("_ ");
+            else printf("%c ", lacuna[i]);
         }
-        printf("\nnumero de tentativas restante: %d\n", 6 -  erro);
-        printf("digite uma letra: ");
-        scanf("%c", &letra);
+
+        printf("\n\nDigite a letra: ");
+        scanf(" %c", &letra);
+
+        int count = 0;
+        for (int i = 0; i < tamanho; i++) {
+            if (palavra[i] == letra && lacuna[i] != letra) {
+                lacuna[i] = letra;
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            printf("Errou a letra.\n");
+            vida--;
+        } else {
+            printf("Parabéns, acertou!\n");
+        }
+
+        if (strcmp(palavra, lacuna) == 0) {
+            v = 1;
+        }
+
+
+        printf("Pressione ENTER para continuar...");
+        getchar(); getchar();
     }
 
-    for(int i = 0; i < tamanho; i++){
-        if(palavra[i] == letra &&resposta[i] == '-' ){
-            resposta[i] = letra; 
-            acerto++;
-            acerto = 1 ;
-        }
-    }
-    if(!acerto){
-        printf("nao acertou.\n");
-    }else{
-        printf("acertouuuuuuu.\n");
-    }
-    if(acerto == tamanho){
-        printf("parabens acertou: %s\n", palavra);
-    }else{
-        printf("perdeu! a palavra era : %s\n", palavra);
+    if (v == 1) {
+        printf("\nParabéns! Você venceu com %d vidas restantes.\n", vida);
+    } else {
+        printf("\nVocê perdeu! A palavra era: %s\n", palavra);
     }
 
     return 0;
